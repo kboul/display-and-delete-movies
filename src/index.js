d2d3e04c1a419c93ec7434f38d36d6927e5af690
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-08-21 17:00:19 +0300

    Install & import font-awesome library

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css'; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
