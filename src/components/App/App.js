a5b02f20f5e3eda47376d6a3cc3dfb1cc74f45e2
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-08-28 14:32:54 +0300

    Implement routing, not-found page, redirect to Movies comp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movies from '../Movies/Movies';
+import Navbar from '../Navbar/Navbar';
+import Customers from '../Customers/Customers'
+import Rentals from '../Rentals/Rentals'
+import NotFound from '../NotFound/NotFound';
+
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class App extends Component {
+	state = {
+		routes: [
+			{ path: '/movies', name: "Movies" },
+			{ path: '/customers', name: "Customers" },
+			{ path: '/rentals', name: "Rentals" }
+		]
+	}
 	render() {
 		return (
-			<Movies />
+			<div>
+				<Navbar routes={this.state.routes} />
+					<main role="main" className="container">
+						<Switch>
+							<Route path="/movies" component={Movies}></Route>
+							<Route path="/customers" component={Customers}></Route>
+							<Route path="/rentals" component={Rentals}></Route>
+							<Route path="/not-found" component={NotFound}></Route>
+							<Redirect from="/" exact to="/movies" />
+							<Redirect to="/not-found" />
+						</Switch>
+					</main>
+				</div>
 		)
 	}
 }
