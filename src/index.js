68a7f6545e8a61c94a1b440de7d17649db29401e
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-08-28 12:58:21 +0300

    Install react-router-dom & wrap App component with Router tag

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -7,5 +9,10 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css'; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root')
+);
 registerServiceWorker();
