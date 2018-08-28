1401889a91e68ddfd3407d76e6846727fee856fe
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-08-28 15:25:14 +0300

    Create a Login comp & add a Login route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import Movies from '../Movies/Movies';
 import Navbar from '../Navbar/Navbar';
 import Customers from '../Customers/Customers'
 import Rentals from '../Rentals/Rentals'
 import NotFound from '../NotFound/NotFound';
 import MovieForm from '../MovieForm/MovieForm';
+import LoginForm from '../LoginForm/LoginForm';
 
-import { Switch, Route, Redirect } from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
 	state = {
 		routes: [
 			{ path: '/movies', name: "Movies" },
 			{ path: '/customers', name: "Customers" },
-			{ path: '/rentals', name: "Rentals" }
+			{ path: '/rentals', name: "Rentals" },
+			{ path: '/login', name: "Login" }
 		]
 	}
 	render() {
@@ -23,6 +26,7 @@ class App extends Component {
 				<Navbar routes={this.state.routes} />
 					<main role="main" className="container">
 						<Switch>
+							<Route path="/login" component={LoginForm} /> 
 							<Route path="/movies/:id" component={MovieForm} />      
 							<Route path="/movies" component={Movies}></Route>
 							<Route path="/customers" component={Customers}></Route>
