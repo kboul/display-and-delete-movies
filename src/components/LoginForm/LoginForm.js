1b726dca081050e8ae8d135a5780b2f3d1a5f707
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-08-28 15:30:41 +0300

    Create a login form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,7 +3,37 @@ import React, { Component } from 'react';
 class LoginForm extends Component {
     render() { 
         return ( 
-            <h1>Login</h1>
+            <div>
+                <h1>Login</h1>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">
+                            Email address
+                        </label>
+                        <input 
+                            type="email" 
+                            className="form-control" 
+                            id="exampleInputEmail1" 
+                            aria-describedby="emailHelp" 
+                            placeholder="Enter email" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">
+                            Password
+                        </label>
+                        <input 
+                            type="password" 
+                            className="form-control"
+                            id="exampleInputPassword1" 
+                            placeholder="Password" />
+                    </div>
+                    <button 
+                        type="submit"
+                        className="btn btn-primary">
+                        Login
+                    </button>
+                </form>
+            </div>
         );
     }
 }
