a9663aad6445a59009c8e3b6be32735818fcd2fc
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-08-29 17:02:41 +0300

    Populate RegisterForm with Name, Password & Username fields

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,10 +1,61 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Form from '../common/Form/Form';
+import Input from '../common/Input/Input';
+import Joi from 'joi-browser';
 
-class RegisterForm extends Component {
-    state = {  }
+class RegisterForm extends Form {
+    state = {
+        data: {
+            username: '',
+            password: '',
+            name: ''
+        },
+        errors: {}
+    }
+
+    schema = {
+        username: Joi.string().email({ minDomainAtoms: 2 }).required().label("Username"),
+        password: Joi.string().min(5).required().label("Password"),
+        name: Joi.string().required().label("Name")
+    }
+
+    doSubmit = () => {
+        // Call the server
+        console.log("Submitted");
+    }
+
     render() { 
+        const { data, errors } = this.state;
         return ( 
-            <h1>Register</h1>
+            <div>
+                <h1>Register</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <Input 
+                        name="username" 
+                        label="Username"
+                        value={data.username}
+                        error={errors.username}
+                        onChange={this.handleChange} />
+                    <Input 
+                        name="password" 
+                        label="Password"
+                        value={data.password}
+                        error={errors.password}
+                        onChange={this.handleChange} />
+                    <Input 
+                        name="name" 
+                        label="Name"
+                        value={data.name}
+                        error={errors.name}
+                        onChange={this.handleChange} />
+                    <button 
+                        disabled={this.validate()}
+                        type="submit"
+                        className="btn btn-primary">
+                        Register
+                    </button>
+                </form>
+            </div>
         );
     }
 }
