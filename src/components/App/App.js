fc9dd9a2325b2db6a852d322b744d8fb3c8a61d6
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-08-30 11:34:30 +0300

    Replace MoviesNew with already existing MovieForm comp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,7 +9,6 @@ import NotFound from '../NotFound/NotFound';
 import MovieForm from '../MovieForm/MovieForm';
 import LoginForm from '../LoginForm/LoginForm';
 import RegisterForm from '../RegisterForm/RegisterForm';
-import MoviesNew from '../MoviesNew/MoviesNew';
 
 import './App.css';
 
@@ -31,7 +30,7 @@ class App extends Component {
 						<Switch>
 							<Route path="/register" component={RegisterForm} /> 
 							<Route path="/login" component={LoginForm} /> 
-							<Route path="/movies/new" component={MoviesNew} />    
+							<Route path="/movies/new" component={MovieForm} />    
 							<Route path="/movies/:id" component={MovieForm} />      
 							<Route path="/movies" component={Movies}></Route>
 							<Route path="/customers" component={Customers}></Route>
