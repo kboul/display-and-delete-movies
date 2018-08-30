16d5c84ff8082d4f72a24dd877e14829e94f9ada
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-08-30 11:12:18 +0300

    Refactor MovieForm's onClick method

--- src/components/MovieForm/MovieForm.js
+++ src/components/MovieForm/MovieForm.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react'
 
 class MovieForm extends Component {
-    handleSave = () => {
-        const { history } = this.props;
-        history.push("/movies");
-    }
-
     render() { 
-        const { match } = this.props;
+        const { match, history } = this.props;
         return (
             <div>
                 <h1>Movie Form {match.params.id}</h1>
                 <button 
                     className="btn btn-primary"
-                    onClick={this.handleSave}>
+                    onClick={() => { history.push('/movies')} }>
                     Save
                 </button>
             </div>
