ed12645c3a64a7111c60d0c5a7b2110aa216fdf2
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-09-12 15:04:27 +0300

    Fix identation on App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -26,21 +26,21 @@ class App extends Component {
 		return (
 			<div>
 				<Navbar routes={this.state.routes} />
-					<main role="main" className="container">
-						<Switch>
-							<Route path="/register" component={RegisterForm} /> 
-							<Route path="/login" component={LoginForm} /> 
-							<Route path="/movies/new" component={MovieForm} />    
-							<Route path="/movies/:id" component={MovieForm} />      
-							<Route path="/movies" component={Movies}></Route>
-							<Route path="/customers" component={Customers}></Route>
-							<Route path="/rentals" component={Rentals}></Route>
-							<Route path="/not-found" component={NotFound}></Route>
-							<Redirect from="/" exact to="/movies" />
-							<Redirect to="/not-found" />
-						</Switch>
-					</main>
-				</div>
+				<main role="main" className="container">
+					<Switch>
+						<Route path="/register" component={RegisterForm} /> 
+						<Route path="/login" component={LoginForm} /> 
+						<Route path="/movies/new" component={MovieForm} />    
+						<Route path="/movies/:id" component={MovieForm} />      
+						<Route path="/movies" component={Movies}></Route>
+						<Route path="/customers" component={Customers}></Route>
+						<Route path="/rentals" component={Rentals}></Route>
+						<Route path="/not-found" component={NotFound}></Route>
+						<Redirect from="/" exact to="/movies" />
+						<Redirect to="/not-found" />
+					</Switch>
+				</main>
+			</div>
 		)
 	}
 }
