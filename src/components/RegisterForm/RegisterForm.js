a5f58b7436ed072d86defb64acffbad691136823
kvoulgaridis <kvoulgaridis@iit.demokritos.gr>
2018-09-19 13:04:34 +0300

    Display DB error message when a user has registered & tries to re-register with same credentialas

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -20,7 +20,17 @@ class RegisterForm extends Form {
     }
 
     doSubmit = async () => {
-       await userService.register(this.state.data);
+        try {
+            await userService.register(this.state.data);
+        } catch(ex) {
+            if (ex.response && ex.response.status === 400) {
+                // if user has already registered & tries to register again
+                // pass the DB output error message to username
+                const errors = {...this.state.errors};
+                errors.username = ex.response.data;
+                this.setState({ errors });
+            }
+        }
     }
 
     render() { 
