797d2d50cb09ab1afc736006d6340593b308cf85
kboul <kos.voulgaridis@gmail.com>
2019-09-23 15:51:32 +0300

    Fix broker pagination style & remove href warning

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import _ from 'lodash'; // optimized underscore library
-import PropTypes from 'prop-types'; 
+import PropTypes from 'prop-types';
+
+const anchorStyle = { cursor: 'pointer', boxShadow: 'none' }
 
 const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
     // calculate required pagination numbers
     const pagesCount = Math.ceil(itemsCount / pageSize);
     // if pagesCount equals 1 don't show pagination
-    if (pagesCount === 1) return null;    
+    if (pagesCount === 1) return null;
     // create an array of required pagination numbers using lodash
     const pages = _.range(1, pagesCount + 1);
 
@@ -15,14 +17,16 @@ const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
             <ul className="pagination justify-content-center">
                 {pages.map(page => {
                     return (
-                        <li 
-                            key={page} 
-                            className={ (page !== currentPage) ? "page-item" : "page-item active"}>
-                            <a 
-                                className="page-link" 
-                                onClick={() => { onPageChange(page)} }>
+                        <li
+                            key={page}
+                            className={(page !== currentPage) ? "page-item" : "page-item active"}>
+                            <a
+                                className="page-link"
+                                style={anchorStyle}
+                                href="/#"
+                                onClick={(event) => { onPageChange(page); event.preventDefault(); }}>
                                 {page}
-                            </a> 
+                            </a>
                         </li>
                     )
                 })}
