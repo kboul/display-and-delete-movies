cfcc6186a4fb2b65cfd72713ac7ccd52ad78479e
kboul <kos.voulgaridis@gmail.com>
2019-09-23 17:12:29 +0300

    Place Like.css under styles folder & delete Like parent folder, add propTypes on Like

---
+++ src/components/Like.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/Like.css';
+
+const Like = ({ like, onClick }) => {
+    let likeStyle = 'fa fa-heart';
+    if (!like) likeStyle += '-o';
+
+    return (
+        <i
+            id="fa"
+            className={likeStyle}
+            aria-hidden="true"
+            onClick={onClick}>
+        </i>
+    );
+}
+
+Like.propTypes = {
+    like: PropTypes.bool,
+    onClick: PropTypes.func.isRequired
+}
+
+export default Like;
--- src/components/MoviesTable/MoviesTable.js
+++ src/components/MoviesTable/MoviesTable.js
@@ -1,43 +1,41 @@
 import React, { Component } from 'react'
-import './MoviesTable.css';
-
-import Like from '../Like/Like';
 import { Link } from 'react-router-dom';
-
+import Like from '../Like';
 import authService from '../../services/authService';
+import './MoviesTable.css';
 
 class MoviesTable extends Component {
     columns = [
         { path: "title", label: "Title" },
-        { path: "genre.name", label: "Genre"},
+        { path: "genre.name", label: "Genre" },
         { path: "numberInStock", label: "Stock" },
         { path: "dailyRentalRate", label: "Range" }
     ]
 
     renderSortIcon = column => {
         const { sortColumn } = this.props;
-        if ( column.path !== sortColumn.path) return null;
+        if (column.path !== sortColumn.path) return null;
         if (sortColumn.order === 'asc') return <i className="fa fa-sort-asc"></i>;
         return <i className="fa fa-sort-desc"></i>
     }
 
-    render() { 
+    render() {
         const { movies, onLikeMovie, onDeleteMovie, onSortMovie } = this.props;
 
-        return (  
+        return (
             <table className="table">
                 <thead>
                     <tr>
-                        { this.columns.map(column => {
+                        {this.columns.map(column => {
                             return (
-                                <th 
-                                    key={column.path} 
-                                    onClick={() => { onSortMovie(column.path)} } 
+                                <th
+                                    key={column.path}
+                                    onClick={() => { onSortMovie(column.path) }}
                                     scope="col">
                                     {column.label} {this.renderSortIcon(column)}
                                 </th>
                             )
-                        }) } 
+                        })}
                         <th></th>
                         <th></th>
                     </tr>
@@ -46,23 +44,23 @@ class MoviesTable extends Component {
                     return (
                         <tbody key={movie._id}>
                             <tr>
-                                <td><Link to={`/movies/${movie._id}`}>{movie.title}</Link></td>  
-                                <td>{movie.genre.name}</td>   
-                                <td>{movie.numberInStock}</td> 
+                                <td><Link to={`/movies/${movie._id}`}>{movie.title}</Link></td>
+                                <td>{movie.genre.name}</td>
+                                <td>{movie.numberInStock}</td>
                                 <td>{movie.dailyRentalRate}</td>
                                 <td>
-                                    <Like 
-                                        like={movie.like} 
-                                        onClick={() => { onLikeMovie(movie) } }/>
+                                    <Like
+                                        like={movie.like}
+                                        onClick={() => { onLikeMovie(movie) }} />
                                 </td>
                                 <td>
-                                    {authService.getCurrentUser() && <button 
+                                    {authService.getCurrentUser() && <button
                                         className="btn btn-danger btn-sm"
                                         onClick={() => onDeleteMovie(movie)}>
                                         Delete
                                     </button>}
                                 </td>
-                            </tr> 
+                            </tr>
                         </tbody>
                     )
                 })}
